451e983056b1df350f6c4b4642f4faf3928f0dff
Nabin Singh <nabin@skilluplabs.com.au>
2024-08-30 20:03:05 +1000

    Added categories component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Categories from "./components/Categories";
+
 function App() {
   return (
-    <>
-      
-    </>
+    <div>
+      <Categories title="Shop by Category" linkUrl="#" linkText="Browse all categories" />
+    </div>
   )
 }
 
