a3d7239c490789f03adb0359293e54ed4311bdba
Nabin Singh <nabin@skilluplabs.com.au>
2024-08-30 20:50:09 +1000

    Added product data

---
+++ src/data/productData.js
@@ -0,0 +1,31 @@
+const productData = {
+    categories: [
+        {
+            id: 1,
+            category: "New Arrivals",
+            image: "https://tailwindui.com/img/ecommerce-images/home-page-01-category-01.jpg"
+        },
+        {
+            id: 2,
+            category: "Productivity",
+            image: "https://tailwindui.com/img/ecommerce-images/home-page-01-category-02.jpg"
+        },
+        {
+            id: 3,
+            category: "Workspace",
+            image: "https://tailwindui.com/img/ecommerce-images/home-page-01-category-04.jpg"
+        },
+        {
+            id: 4,
+            category: "Accessories",
+            image: "https://tailwindui.com/img/ecommerce-images/home-page-01-category-05.jpg"
+        },
+        {
+            id: 5,
+            category: "Sale",
+            image: "https://tailwindui.com/img/ecommerce-images/home-page-01-category-03.jpg"
+        }
+    ]
+}
+
+export default productData;
